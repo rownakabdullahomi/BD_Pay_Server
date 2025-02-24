dd37a4769e57faa5f1a0341059565014e0a42d21
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-02-25 01:31:23 +0600

    create new user done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -11,6 +12,58 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d3h8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection = client.db("bdPayDB").collection("users");
+
+
+
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        // check if the user already exists...
+        const query = { email: user.email , nid: user.nid, phone: user.phone}
+        const existingUser = await userCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: "user already exists", insertedId: null });
+        }
+        const result = await userCollection.insertOne({ ...user, timestamp: Date.now() });
+        res.send(result);
+      })
+
+
+
+
+
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
